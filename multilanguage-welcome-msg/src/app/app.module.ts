a2131056f35525b38c13dbeb0fa0c39fd9e32392
junhobaik <junhobaik@gmail.com>
2018-05-21 02:11:44 +0900

    3.3 머터리얼 적용

--- multilanguage-welcome-msg/src/app/app.module.ts
+++ multilanguage-welcome-msg/src/app/app.module.ts
@@ -6,20 +6,42 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
 import { LangSelectorComponent } from './lang-selector/lang-selector.component';
-import { I18nSupportService } from "./i18n-support.service";
+import { I18nSupportService } from './i18n-support.service';
+import { LangSelectorBtnPipe } from './lang-selector/lang-selector-btn.pipe';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatToolbarModule,
+  MatSnackBarModule,
+  MatCardModule,
+  MatInputModule,
+  MatRadioModule,
+  MatButtonModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeMsgComponent,
-    LangSelectorComponent
+    LangSelectorComponent,
+    LangSelectorBtnPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule
   ],
-  providers: [{provide: COMPOSITION_BUFFER_MODE, useValue: false}, I18nSupportService],
+  providers: [
+    { provide: COMPOSITION_BUFFER_MODE, useValue: false },
+    I18nSupportService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- multilanguage-welcome-msg/src/app/welcome-msg/welcome-msg.component.ts
+++ multilanguage-welcome-msg/src/app/welcome-msg/welcome-msg.component.ts
@@ -2,6 +2,8 @@ import { Component, AfterViewInit } from '@angular/core';
 
 import { I18nSupportService } from '../i18n-support.service';
 
+import { MatSnackBar } from "@angular/material";
+
 @Component({
   selector: 'app-welcome-msg',
   templateUrl: './welcome-msg.component.html',
@@ -13,12 +15,13 @@ export class WelcomeMsgComponent implements AfterViewInit {
   private valid = false;
   private static CHK_KEYUP_WAIT_SEC = 5000;
 
-  constructor(public i18nSupporter: I18nSupportService) {}
+  constructor(public i18nSupporter: I18nSupportService, private snackbar: MatSnackBar) {}
 
   ngAfterViewInit() {
     const checkTouchedFn = () => {
       if (this.valid) return;
       //alert('이름을 입력해 주세요');
+      this.snackbar.open('이름을 입력해주세요', '확인', {duration: 3000})
     };
 
     setTimeout(checkTouchedFn, WelcomeMsgComponent.CHK_KEYUP_WAIT_SEC);
