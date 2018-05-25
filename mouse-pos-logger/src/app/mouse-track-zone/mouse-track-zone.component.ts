ef34322a64b80eb1a5d02312ddf9c19761c6f856
junhobaik <junhobaik@gmail.com>
2018-05-25 21:03:54 +0900

    5.1.2 마우스 위치 로거

---
+++ mouse-pos-logger/src/app/mouse-track-zone/mouse-track-zone.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MySpecialLoggerService } from '../my-special-logger.service';
+import { LogLevel } from '../log-level.enum';
+
+@Component({
+  selector: 'mpl-mouse-track-zone',
+  templateUrl: './mouse-track-zone.component.html',
+  styleUrls: ['./mouse-track-zone.component.css']
+})
+export class MouseTrackZoneComponent implements OnInit {
+  @Input() private logger: MySpecialLoggerService;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  captureMousePos($event: MouseEvent) {
+    this.logger.debug('click event occured');
+    const pos = [$event.clientX, $event.clientY];
+    this.logger.info(`x:${pos[0]} y:${pos[1]}`);
+  }
+}
