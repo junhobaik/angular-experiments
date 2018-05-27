d3af6b0d0436d9e96dd37341ccb3cbbfc8c2ea5e
junhobaik <junhobaik@gmail.com>
2018-05-27 22:20:10 +0900

    mpl 초기화

--- mouse-pos-logger/src/app/app.module.ts
+++ mouse-pos-logger/src/app/app.module.ts
@@ -2,16 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MouseTrackZoneComponent } from './mouse-track-zone/mouse-track-zone.component';
-
-import { MySpecialLoggerService } from './my-special-logger.service';
-import { LogLevel } from './log-level.enum';
-import { LOG_LEVEL_TOKEN } from './app.token';
 
 @NgModule({
-  declarations: [AppComponent, MouseTrackZoneComponent],
-  imports: [BrowserModule],
-  providers: [MySpecialLoggerService, {provide: LOG_LEVEL_TOKEN, useValue: LogLevel.INFO}],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
