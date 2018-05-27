791c2e0fe7898c800fdf5fbddf3663a74b51893c
junhobaik <junhobaik@gmail.com>
2018-05-27 22:30:14 +0900

    mpl, MySpecialLogger 서비스 생성 및 구현

---
+++ mouse-pos-logger/src/app/my-special-logger.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import * as format from 'date-fns/format';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class MySpecialLoggerService {
+  logLevel: LogLevel;
+  logs: string[] = [];
+  private readonly MAX_HISTORY_CNT: number = 100;
+  private readonly TIME_FORMATTER: string = 'YYYY-MM-DD HH:mm:ss.SSS';
+
+  constructor(logLevel: LogLevel) {
+    this.logLevel = logLevel;
+  }
+}
